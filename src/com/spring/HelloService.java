ad80c06dab04b87cc6068095bf8ecd96b79ff4ff
lsh <522097606@qq.com>
2019-05-04 20:09:55 +0800

    my springdemo1

---
+++ src/com/spring/HelloService.java
@@ -0,0 +1,18 @@
+package com.spring;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by LSH on 2019/5/3 - 12:45.
+ * <p>
+ * declaration :
+ */
+public class HelloService {
+
+    public String check(String name,String password){
+        if(StringUtils.isNoneBlank(name,password)){
+            return "welcome";
+        }
+        return "no pass!";
+    }
+}
---
+++ web/WEB-INF/test1.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: LSH
+  Date: 2019/5/4
+  Time: 0:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>test1</title>
+</head>
+<body>
+呵呵呵呵
+</body>
+</html>
